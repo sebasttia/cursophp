bbaa991715ea12c37d39e6508368a19515e7d156
sebasttia <144245136+sebasttia@users.noreply.github.com>
2023-12-23 09:23:21 -0400

    add+

--- desafio011/index.php
+++ desafio011/index.php
@@ -8,24 +8,37 @@
 </head>
 <body>
     <?php 
-    
+        $preco = $_REQUEST['preco'] ?? '0';
+        $reaj = $_REQUEST['reaj'] ??'0';
     ?>
     <main>
         <h1>Reajustador de Preços</h1>
         <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
             <label for="preco">Preço do Produto (R$)</label>
-            <input type="number" name="preco" id="preco" min="0.10" step="0.01">
-            <label for="reaj">Qual será o percentual de reajuste? <strong><span id="p">?%</span></strong></label>
-            <input type="range" name="reaj" id="reaj" min="0" max="100" step="1">
+            <input type="number" name="preco" id="preco" min="0.10" step="0.01" value="<?=$preco?>">
+            <label for="reaj">Qual será o percentual de reajuste? <strong><span id="p">?</span>%</strong></label>
+            <input type="range" name="reaj" id="reaj" min="0" max="100" step="1" oninput="mudaValor()" value="<?=$reaj?>">
             <input type="submit" value="Reajustar">
         </form>
     </main>
+    <?php 
+        $aumento = $preco * $reaj / 100;
+        $novo = $preco + $aumento; 
+    ?>
     <section>
         <?php 
         
         ?>
         <h2>Resultado do Reajuste</h2>
-        <p>O produto que custava R$[], com <strong>[] de aumento</strong> Vai passar a custar <strong>R$[]</strong> a partir de agora.</p>
+        <p>O produto que custava R$<?=number_format($preco, 2, ",",".")?> com <strong><?=$reaj?>% de aumento</strong> Vai passar a custar <strong>R$<?=number_format($novo, 2, ",",".")?></strong> a partir de agora.</p>
     </section>
+    <script>
+
+        mudaValor()
+
+        function mudaValor() {
+            p.innerText = reaj.value
+        }
+    </script>
 </body>
 </html>
